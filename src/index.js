976f29c11da0cfa7b040ebdd2efcdbe2e9cf713c
yzycool <2596915059@qq.com>
2021-03-10 14:30:26 +0800

    feat:创建项目

---
+++ src/index.js
@@ -0,0 +1,61 @@
+class ScreenStatus {
+    constructor(options = {}) {
+      this.init(options)
+    }
+    init(options) {
+      this.listenScreen()
+      this.events = { blur: [], focus: [] }
+      this.handlerError = options.handlerError || console.error;
+    }
+    listenScreen() {
+      this.blurNotification = this.creatNotification('blur')
+      this.focusNotification =  this.creatNotification('focus')
+      this.listenChange = ()=>{
+        if (document.visibilityState === 'hidden') {
+             this.blurNotification()
+        }
+        if (document.visibilityState === 'visible') {
+        this.focusNotification()
+        }
+       }
+        document.addEventListener('visibilitychange', this.listenChange)
+    }
+    creatNotification = (type) => {
+      if (type || this.events[type]){
+       return()=>{
+        this.events[type].forEach(fn => {
+          try {
+            fn()
+          } catch(error) {
+            this.handlerError(error)
+          }
+        })
+      }
+    }
+    }
+    creatEvents(type, handler) {
+      if (!handler || typeof handler !== 'function') return
+      this.events[type].push(handler)
+      return () => {
+        removeEvents(type, handler)
+      }
+    }
+    removeEvents(type, handler) {
+      if (!handler || typeof handler !== 'function') return
+      handler = this.events[type]
+      _index = handler.indexOf(handler)
+      if(_index !== -1){
+            handler.splice(_index,1) 
+      }
+    }
+    onBlur(handler) {
+      return this.creatEvents('blur', handler)
+    }
+    onShow(handler) {
+      return this.creatEvents('focus', handler)
+    }
+    destroy(){
+      this.events = null
+      window.removeEventListener('visibilitychange', this.showHandler, false)
+    }
+  }
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'umd',
+        filename: 'listenScreen.js'
+    },
+
+    devServer: {
+        hot: true,
+        open: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'postcss-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
+    },
+
+
+    plugins: [new HtmlWebpackPlugin({ // 打包输出HTML
+        title: 'Hello World app',
+        minify: { // 压缩HTML文件
+            removeComments: true, // 移除HTML中的注释
+            collapseWhitespace: true, // 删除空白符与换行符
+            minifyCSS: true// 压缩内联css
+        },
+        filename: 'index.html',
+        template: 'index.html'
+    })]
+};
+
