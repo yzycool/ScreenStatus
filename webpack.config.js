0874cc6505d72274f0ab7a5e59b97f9f1b792277
yzycool <2596915059@qq.com>
2021-03-15 10:25:32 +0800

    feat:优化代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -8,12 +7,6 @@ module.exports = {
         libraryTarget: 'umd',
         filename: 'ScreenStatus.js'
     },
-
-    devServer: {
-        hot: true,
-        open: true
-    },
-
     module: {
         rules: [
             {
@@ -21,36 +14,7 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/
             },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    { loader: 'css-loader', options: { importLoaders: 1 } },
-                    'postcss-loader',
-                    'less-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
         ]
-    },
-
-
-    plugins: [new HtmlWebpackPlugin({ // 打包输出HTML
-        title: 'Hello World app',
-        minify: { // 压缩HTML文件
-            removeComments: true, // 移除HTML中的注释
-            collapseWhitespace: true, // 删除空白符与换行符
-            minifyCSS: true// 压缩内联css
-        },
-        filename: 'index.html',
-        template: 'index.html'
-    })]
+    }
 };
 
